0c96cdf18962cf895860349ea20ed2c28663c6f3
Viestar <m.silverbuckson@gmail.com>
2022-12-20 07:07:38 +0300

    Added dependencies

--- gui.py
+++ gui.py
@@ -10,7 +10,7 @@ from tkinter import Tk, Canvas, Entry, Text, Button, PhotoImage
 
 OUTPUT_PATH = Path(__file__).parent
 ASSETS_PATH = OUTPUT_PATH / Path(
-    r"C:\Users\DELL\OneDrive\Documents\Silcodes\PycharmProjects\UltimateHoldingsLimited\build\assets\frame0")
+    r"build\assets\frame0")
 
 
 def relative_to_assets(path: str) -> Path:
